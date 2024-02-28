25b01f1471ba2989665f99ebd50ca51655dc37c0
ErickLessingDev <ericklessingdev@gmail.com>
2024-02-28 15:37:52 +0200

    escaped '

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           </h1>
           <h2 className="text-[24px] md:text-[50px] font-semibold text-transparent text-shadow bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-200 kode-mono-700" >FULL STACK DEVELOPER</h2>
           <p className="text-[14px] md:text-[16px] text-gray-200 mt-4 md:mt-8 md:block kode-mono-400" >
-            I'm a Full Stack Developer with a passion for creating beautiful and functional web applications. I have experience with a variety of technologies and frameworks, and I'm always looking to learn more.
+            I&apos;m a Full Stack Developer with a passion for creating beautiful and functional web applications. I have experience with a variety of technologies and frameworks, and I&apos;m always looking to learn more.
           </p>
         </div>
   )
